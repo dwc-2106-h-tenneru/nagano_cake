7eed3cfc677f5936931b294cc974af53745fc256
EC2 Default User <ec2-user@ip-172-31-49-65.ec2.internal>
2021-07-14 04:02:30 +0000

    masayan

---
+++ app/controllers/masayan_controller.rb
@@ -0,0 +1,2 @@
+class MasayanController < ApplicationController
+end
---
+++ test/controllers/masayan_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MasayanControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
